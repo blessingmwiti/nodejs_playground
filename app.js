b374aa3efbb99181867c5117ef049ca6b3272391
blessingmwiti <blessingmwiti>
2023-10-18 21:03:23 +0300

    update

--- app.js
+++ app.js
@@ -8,12 +8,23 @@ emitter.on('Log', (arg) => {
 
 emitter.emit('Log', {name: 'Blessing'});
 
-
-
 const Logger = require('./logger');
 const logger = new Logger();
 
 logger.on('add', () => {
     console.log('Exported module called');
 })
-logger.add(4, 6); 
+logger.add(4, 6);
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.write('Hello muchacho');
+        res.end();
+    }
+});
+
+
+server.listen(3000);
+console.log('Listening on port 3000...');
